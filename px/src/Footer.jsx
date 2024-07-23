fa707a8112e1e182e96f60baa3b375c345498de1
KarlP17123 <45360159+KarlP17123@users.noreply.github.com>
2024-07-23 17:56:51 +0000

    add the start files

---
+++ px/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return(
+        <footer>
+            <h4>The Power Drinks</h4>
+             <p>&copy; {new Date().getFullYear()} Your website name</p>
+        </footer>
+    );
+}
+
+export default Footer
